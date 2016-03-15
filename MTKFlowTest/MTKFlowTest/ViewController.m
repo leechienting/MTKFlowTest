463725b1888c4738096de2b03b10aabdda58f39c
leechienting <leechienting@gmail.com>
2016-03-15 13:09:16 +0800

    edit ViewController.m

--- MTKFlowTest/MTKFlowTest/ViewController.m
+++ MTKFlowTest/MTKFlowTest/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-	NSLog(@"11");
+	NSLog(@"22");
 }
 
 - (void)didReceiveMemoryWarning {
